ce9afa3ceab8fb30e8650896d4b29b3c5f6b5a3d
juan-josue <juansemail2001@gmail.com>
2024-08-13 16:53:26 -0400

    feat: add daisyui and custom theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,33 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      {
+        ornipedia: {
+          primary: "#22c55e",
+
+          secondary: "#f3f4f6",
+
+          accent: "#eab308",
+
+          neutral: "#f3f4f6",
+
+          "base-100": "#292524",
+
+          info: "#f3f4f6",
+
+          success: "#22c55e",
+
+          warning: "#fcd34d",
+
+          error: "#f43f5e",
+        },
+      },
+    ],
+  },
+};
