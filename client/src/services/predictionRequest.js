1655b45ea9c64a636d067075be0858d0dca5349e
juan-josue <juansemail2001@gmail.com>
2024-08-21 02:37:53 -0400

    feat: handle errors in prediction request

--- client/src/services/predictionRequest.js
+++ client/src/services/predictionRequest.js
@@ -7,6 +7,30 @@ export default async function predictionRequest(image_url) {
     });
     return response.data;
   } catch (error) {
-    console.error(error.message);
+    // Handle http errors
+    if (error.response) {
+      const status = error.response.status;
+
+      if (status === 400) {
+        console.error(
+          "Bad Request, invalid or no url provided: ",
+          error.response.data.error
+        );
+      } else if (status === 404) {
+        console.error(
+          "Could not predict species from url ",
+          error.response.data.error
+        );
+      } else if (status === 500) {
+        console.error("Server Error: ", error.response.data.error);
+      } else {
+        console.error("Error predicting species:", error.response.data.error);
+      }
+    } else {
+      // Handle errors made making the request
+      console.error("Error setting up the prediction request:", error.message);
+    }
+
+    throw error;
   }
-};
+}
